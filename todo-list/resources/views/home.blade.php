44a21746bfb62c8a93f26240ede4cc5d2b3b76c5
Hamas Aksoy <2422120010@iesu.edu.tr>
2025-08-19 16:19:00 +0300

    update home.blade.php

--- todo-list/resources/views/home.blade.php
+++ todo-list/resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8"><!--بيخلي الصفحة تقدر تعرض أي لغة-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,32 +8,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!--  html بال  cssربط ملف ال -->
-     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app.css">
 
     <title>Home</title>
     <style>
-        body{
+        body {
             background-image: url('{{ asset("/imgs/background1.jpg")}}');
             background-color: beige;
         }
-        .title{
+
+        .title {
             color: brown;
             background-color: ;
-            margin: 0.50em; /**/
-            font-size: 100px;  /*حجم الخط */
+            margin: 0.50em;
+            /**/
+            font-size: 100px;
+            /*حجم الخط */
             text-align: center;
         }
 
-        form{/*ده حاويه كبيره عطيناها مكان عشان كل الي فيها يكونو في نفس  السطر */
-            float:left;
+        form {
+            /*ده حاويه كبيره عطيناها مكان عشان كل الي فيها يكونو في نفس  السطر */
+            float: left;
             padding: 70px;
             color: brown;
             font-size: 20px;
         }
-        .port{
-            font-size:20px;
+
+        .port {
+            font-size: 20px;
         }
-        
     </style>
 
 </head>
@@ -40,32 +45,41 @@
 <body>
     <h1 class="title">login</h1>
 
-    <!-- login for admin --> 
-    <form class="admin" action="admin.blade.php" method="post"> 
+    <!-- login for admin -->
+    <form class="admin" action="admin.blade.php" method="post">
         <h2 style="text-align:center; font-size:50px;">Admin</h2>
         <p class="port">
-            Name:<input type = "text" name="name"><br><br>
-            Password:<input type = "text" pass="pass"><br><br>
-            <input type="submit" style="font-size:30px; color: brown;"> 
+            <label>Name:</label><!--input descriptions-->
+            <input type="text" placeholder="Name"><br><br>
+            <label>Email:</label>
+            <input type="email" placeholder="email"><br><br>
+            <label>Password:</label>
+            <input type="password" required placeholder="Password"><br><br>
+            <input type="submit" style="font-size:30px; color: brown;">
         </p>
 
-    <!-- المفروض السطر ده هيكون في الكنترولر --> 
-    <!-- <a style="font-size:30px; color:brown" href="http://localhost/todo-list/public/admin"><button>submit</button> </a> -->
+        <!-- المفروض السطر ده هيكون في الكنترولر -->
+        <!-- <a style="font-size:30px; color:brown" href="http://localhost/todo-list/public/admin"><button>submit</button> </a> -->
     </form>
-    
+
 
 
     <!-- login for user -->
-    <form class="user" action="user.blade.php" method="post"> 
+    <form class="user" action="user.blade.php" method="post">
         <h2 style="text-align:center; font-size:50px;">User</h2>
         <p class="port">
-            UserName:<input type = "text" name="name"><br><br>
-            Password:<input type = "text" pass="pass"><br><br>
+            <label>Name:</label><!--input descriptions-->
+            <input type="text" placeholder="Name"><br><br>
+            <label>Email:</label>
+            <input type="email" placeholder="email"><br><br>
+            <label>Password:</label>
+            <input type="password" required placeholder="Password"><br><br>
             <input type="submit" style="font-size:30px; color: brown;">
         </p>
-    <!-- المفروض السطر ده هيكون في الكنترولر--> 
-    <!-- <a href="http://localhost/todo-list/public/user"><button>submit</button> </a>--> 
+        <!-- المفروض السطر ده هيكون في الكنترولر-->
+        <!-- <a href="http://localhost/todo-list/public/user"><button>submit</button> </a>-->
     </form>
 
 </body>
+
 </html>
